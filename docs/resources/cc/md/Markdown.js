f6142613843b11ece1e857c8fded56d351a599e7
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-03-20 16:14:36 +0100

    build

--- docs/resources/cc/md/Markdown.js
+++ docs/resources/cc/md/Markdown.js
@@ -1,13 +1,10 @@
-sap.ui.define(["sap/ui/core/Control", "./marked/marked.min"], (Control /*, marked */) => {
+sap.ui.define(["sap/ui/core/Control", "./marked/marked.min"], (Control /*, marked */ ) => {
     return Control.extend("cc.md.Markdown", {
         metadata: {
             properties: {
                 content: { type: "string", defaultValue: "", bindable: "bindable" },
                 fromFile: { type: "string", defaultValue: "", bindable: "bindable" }
             }
-        },
-        init() {
-            console.debug(`[${this.getMetadata().getName()}] > init`)
         },
         renderer: {
             apiVersion: 2, // high-perf!
@@ -22,6 +19,10 @@ sap.ui.define(["sap/ui/core/Control", "./marked/marked.min"], (Control /*, marke
                 oRM.openStart("div", oControl)
                 oRM.openEnd()
                 const sMarkdown = oControl.getContent()
+                if (!sMarkdown && !oControl.getFromFile()) {
+                    console.debug(`[${oControl.getMetadata().getName()}] > no content available`)
+                    oRM.close("div")
+                    return }
                 if (sMarkdown) {
                     // eslint-disable-next-line no-undef
                     const sHtml = marked(sMarkdown)
@@ -33,7 +34,8 @@ sap.ui.define(["sap/ui/core/Control", "./marked/marked.min"], (Control /*, marke
                         .then((md) => oControl.setContent(md)) // make md available (via .getContent()) to the rendering cycle
                         .catch((err) => console.error(`[${oControl.getMetadata().getName()}] > ERR: ${err}`))
                 }
-            }
+            },
+
         }
     })
 })
