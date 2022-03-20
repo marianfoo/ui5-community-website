3d6a69cca75cce589372eeb496611e2b99c660a6
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-03-20 15:25:08 +0100

    build

--- docs/resources/sap/ui/Global-dbg.js
+++ docs/resources/sap/ui/Global-dbg.js
@@ -77,7 +77,7 @@ sap.ui.define([
 		 */
 		version: "1.96.6",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20220320-1514" }
+		buildinfo : { lastchange : "", buildtime : "20220320-1524" }
 	});
 
 	var oCfgData = window["sap-ui-config"] || {};
